a7834e7e33fd1cff0a823927cb10b0e9ca55f359
quangnst <quangnst@gmail.com>
2020-11-01 21:14:57 +0700

    add source

---
+++ nuxt.config.js
@@ -0,0 +1,60 @@
+module.exports = {
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: "nuxt-auth",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "Nuxt.js project" }
+    ],
+    link: [
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        rel: "stylesheet",
+        href:
+          "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css"
+      }
+    ]
+  },
+  /*
+   ** Customize the progress bar color
+   */
+  loading: { color: "#3B8070" },
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** Run ESLint on save
+     */
+    extend(config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: "pre",
+          test: /\.(js|vue)$/,
+          loader: "eslint-loader",
+          exclude: /(node_modules)/
+        });
+      }
+    }
+  },
+
+  axios: {
+    baseURL: "http://localhost:8080/api"
+  },
+  auth: {
+    strategies: {
+      local: {
+        endpoints: {
+          login: { url: "auth/signin", method: "post", propertyName: "data.token" },
+          user: { url: "test/user", method: "get", propertyName: "data" },
+          logout: false
+        }
+      }
+    }
+  },
+  modules: ["@nuxtjs/axios", "@nuxtjs/auth"]
+};
+
